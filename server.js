0af7f4fb30e979eee203f6eedd441b5ead51c918
Christian Eckenrode <chriseckenrode@me.com>
2017-08-21 00:32:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.listen(PORT, function() {
